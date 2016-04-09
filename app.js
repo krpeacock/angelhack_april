ff36ecb7edc6d939de91f7d4e8f03736abad2016
krpeacock <kylpeacock@gmail.com>
2016-04-09 15:59:54 -0700

    route js files added

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 
-
+// login && index 
 var routes = require('./routes/index');
+
+
 var receipts = require('./routes/receipts');
 var lists = require('./routes/lists');
 var pantry = require('/routes/pantry');
-
 var users = require('./routes/users');
 
 var app = express();
