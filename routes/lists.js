8f4616b81656ccd670cdb53211beb775469ad856
krpeacock <kylpeacock@gmail.com>
2016-04-09 16:12:10 -0700

    pantry.js

---
+++ routes/lists.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../db/knex.js');
+var methodOverride = require('method-override');
+require('locus');
+
+/* GET home page. */
+router.get('/new', function(req, res, next) {
+  res.render('lists/new');
+});
+
+router.get('/:id', function(req, res, next) {
+  res.render('lists/show');
+});
+
+
+
+module.exports = router;
---
+++ routes/pantry.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../db/knex.js');
+var methodOverride = require('method-override');
+require('locus');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('pantry/index');
+});
+
+
+
+module.exports = router;
