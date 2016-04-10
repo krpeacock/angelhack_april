4fff2ac2a6f706770c95fae6a51645d254a2eba5
Wesley Reardan <wesley@4kdev.com>
2016-04-09 17:28:43 -0700

    Add user buys ingredient.

--- migrations/20160409164404_user_buys_ingredient.js
+++ migrations/20160409164404_user_buys_ingredient.js
@@ -1,7 +1,15 @@
 
 exports.up = function(knex, Promise) {
+  return knex.schema.createTable('user_buys_ingredient', function(table) {
+    table.integer('user_id').unsigned().references('id').inTable('users').onDelete('cascade');
+    table.increments();
+    table.decimal('quantity');
+    table.string('quantity_units');
+    table.date('date');
+    table.integer('ingredient_id').unsigned().references('id').inTable('ingredients').onDelete('cascade');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('user_buys_ingredient');
 };
