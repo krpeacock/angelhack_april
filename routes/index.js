c2203bc7085f173b5902da3918f192f3349c45e4
Wesley Reardan <wesley@4kdev.com>
2016-04-09 19:25:25 -0700

    Add login ability.

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,43 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex.js');
 var methodOverride = require('method-override');
+var crypto = require('crypto');
 require('locus');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'View Your Reciepts' });
+  res.render('login', { title: 'View Your Reciepts' });
+});
+
+// Login Request
+router.post('/', function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+  var password_hash = crypto.createHash('sha256').update(password).digest('base64');
+  knex.select('password')
+    .from('users')
+    .where({username: username})
+    .then(function(user_list) {
+      if(user_list.length > 0) {
+        var user = user_list[0];
+        if(user.password != password_hash) {
+          res.send('Error: invalid password.');
+          //res.redirect('#')
+          return;
+        }
+        res.redirect('/home');
+      }
+      // No user: Create User
+      else {
+        console.log('Creating user "' + username + '"');
+        knex('users').insert({
+          username: username,
+          password: password_hash
+        }).then(function() {
+          res.redirect('/home');
+        });
+      }
+    })
 });
 
 
