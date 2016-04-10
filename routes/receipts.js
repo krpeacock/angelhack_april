44774f2474577f5312a24aa0159dc8ae356eb500
krpeacock <kylpeacock@gmail.com>
2016-04-09 21:52:42 -0700

    working on file upload

--- routes/receipts.js
+++ routes/receipts.js
@@ -3,6 +3,11 @@ var router = express.Router();
 var knex = require('../db/knex.js');
 var methodOverride = require('method-override');
 var http = require('https');
+var hod = require('havenondemand');
+var multer = require('multer');
+var upload = multer({dest: '/tmp'});
+
+client = new hod.HODClient('fe962457-0f26-424a-bd97-663116f742fd','v1');
 require('locus');
 
 
@@ -20,19 +25,27 @@ function text_filter(data) {
 }
 
 // First Callback
-function text_callback(req,res,next){
+var text_callback = function(req, res){
+  eval(locus)
+  var image = req.body.image;
+  client.call('ocrdocument', image).on('data', text_filter);
+};
+
+
+
+/*function text_callback(req,res,next){
   var text_back = new Promise(function(resolve, reject) {
-    eval(locus)
     http.post({
       hostname: 'api.havenondemand.com',
       port: '443',
       path: '/1/fe962457-0f26-424a-bd97-663116f742fd/sync/ocrdocument/v1',
     }, function(response){
+      eval(locus)
        res.on('data', text_filter);
     });
   });
-}
+}*/
 
-router.post('/new', text_callback);
+router.post('/', upload.single('image'), text_callback);
 
 module.exports = router;
